8768289ffc71e917bc8ea2b5d92989c992547387
Mark <mverleg.noreply@gmail.com>
2018-01-11 20:40:38 +0100

    gradle run now runs both JVM and JS succesfully!

--- commonmod/src/main/kotlin/demo/Main.kt
+++ commonmod/src/main/kotlin/demo/Main.kt
@@ -3,7 +3,6 @@ package demo
 import demo.commonmod.getPlatform
 
 public fun main(args: Array<String>) {
-    println("hello from ${getPlatform()}")
+    println("Hello from ${getPlatform()}")
 }
-
 
