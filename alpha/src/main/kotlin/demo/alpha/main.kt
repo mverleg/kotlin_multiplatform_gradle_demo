74c391b135b5e73683855385c14040f288fdb804
mark <mark@rafiki>
2018-01-18 21:25:47 +0100

    move js output directory

--- alpha/src/main/kotlin/demo/alpha/main.kt
+++ alpha/src/main/kotlin/demo/alpha/main.kt
@@ -4,7 +4,8 @@ import demo.beta.getPlatform
 import demo.gamma.multiplatform
 
 fun main(args: Array<String>) {
-    println("Hello from ${getPlatform()} (also, ${multiplatform()})")
+    multiplatform()
+    println("Hello from ${getPlatform()}")
 }
 
 
