ee40df1f75a5a51e4e90b8cafa78fd9a790d2352
Mark <mverleg.noreply@gmail.com>
2018-01-20 23:56:53 +0100

    still java tests cannot see their code, I might quit

--- beta/beta-jvm/src/test/kotlin/demo/beta/BetaTest.kt
+++ beta/beta-jvm/src/test/kotlin/demo/beta/BetaTest.kt
@@ -3,7 +3,7 @@ package demo.beta
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
-actual class PlatformTest actual constructor() {
+actual class BetaTest actual constructor() {
     @Test
     actual fun testGetPlatform() {
         assertTrue { "jvm" in Beta().getPlatform().toLowerCase() }
